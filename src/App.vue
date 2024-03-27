5549e32d8589c2a4cab8967eed151b57845851c6
hayatroid <hayatroid@gmail.com>
2024-03-27 18:52:12 +0900

    Add titile scene

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Title from './components/Title.vue'
 import Game from './components/Game.vue'
 
 const id = ref<number>(0)
+const is_playing = ref<boolean>(false)
+
+const gameStart = () => {
+  id.value = 0
+  is_playing.value = true
+}
 </script>
 
 <template>
-  <Game :id="id" :key="id" @complete="id++" />
+  <Title v-if="!is_playing" @game-start="gameStart()" />
+  <Game v-else :id="id" :key="id" @complete="id++" />
 </template>
 
 <style scoped>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+const emits = defineEmits<{
+  gameStart: []
+}>()
 </script>
 
 <template>
+  <button @click="emits('gameStart')">ゲームスタート！</button>
 </template>
 
 <style scoped>
