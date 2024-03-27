71c60d817692dc66daf01431324346bb1583f18a
hayatroid <hayatroid@gmail.com>
2024-03-27 17:57:37 +0900

    Calc start, pos, end

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,8 +5,13 @@ const props = defineProps<{
   id: number
 }>()
 
+const word = idioms[props.id].word
 const idiom_en = idioms[props.id].idiom_en
 const idiom_ja = idioms[props.id].idiom_ja
+
+const start = idiom_en.indexOf(word)
+const pos = start + 1
+const end = start + word.length
 </script>
 
 <template>
